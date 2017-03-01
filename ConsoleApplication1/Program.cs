244fcfc1f9ebd97aeb7846904ffe6014c88c30b6
Alex <Alex@DESKTOP-AITTL8D>
2017-03-01 20:46:40 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+            private static void Main(string[] args)
+        {
+            string chaine = "12,25";
+            int i = Convert.ToInt32(chaine);
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ LE JEU DU PLUS OU DU MOINS/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace LE_JEU_DU_PLUS_OU_DU_MOINS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombreATrouver = new Random().Next(1, 100);
+            int essais = 0;
+            bool faux = false;
+            Console.WriteLine("Saisissez un nombre entre 0 et 100.");
+            while (!faux)
+            {
+                string saisie = Console.ReadLine();
+                int numerique;
+                if (int.TryParse(saisie, out numerique))
+                {
+                    if (numerique<0 || numerique>100)
+                    {
+                        Console.WriteLine("La valeur saisie n'est pas entre 0 et 100");
+                        essais++;
+                    }
+                    else
+                    {
+                        if (numerique<nombreATrouver)
+                        {
+                            Console.WriteLine("C'est plus...");
+                            essais++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("C'est moins...");
+                            essais++;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("La valeur saisie n'est pas entre 0 et 100");
+                    essais++;
+                }
+                if (numerique == nombreATrouver)
+                {
+                    faux = true;
+                }
+            }
+            Console.WriteLine("Vous avez trouvé " + nombreATrouver);
+        }
+    }
+}
